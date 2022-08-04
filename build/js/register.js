1acff85ad1a546fdf45ecd2da3fd0c8c5f3cfb58
DerenikPetrosyan <deren_petrosyan@mail.ru>
2022-08-05 02:40:32 +0400

    change server.js file name register.js

--- build/js/register.js
+++ build/js/register.js
@@ -1,2 +1,2 @@
 "use strict";document.querySelector("form").addEventListener("submit",(function(e){console.log("server js file handleSubmit function"),e.preventDefault();var t=new FormData(e.target);Object.fromEntries(t.entries()),window.location="index.html"}));
-//# sourceMappingURL=server.js.map
+//# sourceMappingURL=register.js.map
